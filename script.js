f693133105e168b01ac4c9497c40b9ed41f0e4ac
ScheckJ <scheckjah@yahoo.com>
2024-04-21 16:51:48 -0400

    appended api data to webpage

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const testBtn = document.querySelector("#test");
-const sectionEl = document.querySelector("#city-display");
+const sectionEl1 = document.querySelector("#city-display");
+const sectionEl2 = document.querySelector('#forecast')
 const apiKey = "024581bdd94ff8a08d079fac8b68ab5d";
 
 // create function that will capture the value of the input the user typed in
@@ -38,7 +39,7 @@ function currentWeatherCard(cityData){
     humidity.textContent = cityData.main.humidity
 
     divEl.append(titleEl, temp, wind, humidity)
-    sectionEl.appendChild(divEl)
+    sectionEl1.appendChild(divEl)
 
 }
 
@@ -60,6 +61,21 @@ function fetchForecastData(city) {
     for (let i = 3; i < forecastData.list.length; i += 8) {
         const element = forecastData.list[i];
         console.log(element);  
+        
+    const divEl = document.createElement('div')
+    const dateEl = document.createElement('h3')
+    const temp = document.createElement('p')
+    const wind = document.createElement('p')
+    const humidity = document.createElement('p')
+        
+    console.log(forecastData.list[i].main);
+    dateEl.textContent = forecastData.list[i].dt_txt
+    temp.textContent = forecastData.list[i].main.temp
+    wind.textContent = forecastData.list[i].wind.speed
+    humidity.textContent = forecastData.list[i].main.humidity
+
+    divEl.append(dateEl, temp, wind, humidity)
+    sectionEl2.appendChild(divEl)
 
       }
   }
